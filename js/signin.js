2b0ef0d667b52ee8f798bc275990c0e8f4eb2fbe
Grober Gonzales <GonzalesDLCGrober@gmail.com>
2022-11-04 14:53:18 -0500

    Link Signin Login

---
+++ js/signin.js
@@ -0,0 +1,9 @@
+(()=>{
+
+const btnTengoCuenta = document.querySelector("#form-btn-tengocuenta")
+
+btnTengoCuenta.addEventListener("click", ()=>{
+    window.location.href = "/login.html"
+})
+
+})()
