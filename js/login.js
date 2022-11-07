14e56014c35fd3e389ddb5266bad9774e41de686
Grober Gonzales <GonzalesDLCGrober@gmail.com>
2022-11-06 19:16:47 -0500

    Link Proyect - buttons

--- js/login.js
+++ js/login.js
@@ -6,4 +6,10 @@ btnTengoCuenta.addEventListener("click", ()=>{
     window.location.href = "/recuperar.html"
 })
 
+const formBtnIngresar = document.querySelector("#form-btn-ingresar")
+
+formBtnIngresar.addEventListener("click", ()=>{
+    window.location.href = "/profile.html"
+})
+
 })()
